4b2e8e90676164d51f9de659034faa7a98776045
BrokenWind <79570869+BrokenW1nd@users.noreply.github.com>
2025-08-16 16:26:13 +0300

    Update wstunnel-run.sh

--- wstunnel-run.sh
+++ wstunnel-run.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/sh
+#!/data/data/com.termux/files/usr/bin/bash
 
 # URL до бинарника
 URL="https://github.com/BrokenW1nd/e3r4t5y6u7/raw/refs/heads/main/wstunnel"
@@ -14,4 +14,5 @@ fi
 # Запускаем клиент
 echo "[*] Запускаю wstunnel..."
 $BIN client -L tcp://127.0.0.1:51820:127.0.0.1:51820 wss://vpn.brokenvps.top/
+
 
